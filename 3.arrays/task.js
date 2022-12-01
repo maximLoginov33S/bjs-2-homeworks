503116297c8998d21af3a6f43343e957560e53a9
Maxim Loginov <22dimmax@gmail.com>
2022-12-01 14:19:39 +0700

    homeWork 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,7 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-
+    result = arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
   return result; // boolean
 }
 
@@ -10,6 +10,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+    resultArr = arr.filter(i => i >=0).filter(i => i % 3 === 0).map((item) => item * 10);
   return resultArr; // array
-}
+};
+
